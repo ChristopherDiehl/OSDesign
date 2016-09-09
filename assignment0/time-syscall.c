efc797b0a845f5148a0cf5438cf9962d521ad55b
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-09-09 19:35:30 -0400

    Time Syscall finished

---
+++ assignment0/time-syscall.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/time.h>
+#define SYSCALLS_TO_PERFORM 100000
+int main(int argc, char ** argv)
+{
+	int i = 0;
+	struct timeval start,end;
+	double total_time = 0.0;
+
+	gettimeofday(&start,NULL);
+
+	for (i = 0; i < SYSCALLS_TO_PERFORM; i ++) {
+		getpid();
+	}
+
+	gettimeofday(&end,NULL);
+
+	total_time = (end.tv_usec - start.tv_usec);
+
+
+	printf("Syscalls Performed: %d\n",i);
+	printf("Total Elapsed Time: %f ms\n",total_time);
+	printf("Average Time Per Syscall: %f ms\n",total_time / SYSCALLS_TO_PERFORM);
+
+	return 1;
+}
